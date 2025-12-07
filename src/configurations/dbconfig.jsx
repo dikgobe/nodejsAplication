9223678a8609f6c446b3ccfb1c8cf37c9d7a0035
dikgobe <djmolepo@gmail.com>
2025-12-07 12:11:26 +0200

    Connected to mango db

---
+++ src/configurations/dbconfig.jsx
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost:27017/jwt_bb",);
+
+mongoose.connection.on('connected',()=>{
+    console.log("Connected to MongoDB Successfully");
+});
+
+mongoose.connection.on('error',(err)=>{
+    console.log("Error while connecting to MongoDB", err);
+});
